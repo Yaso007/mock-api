3a2cd9ed15c6655a21f6b7837ba91b881f611d63
Yaso007 <yashopmukhiazz007@gmail.com>
2026-05-18 10:55:27 +0530

    feat: added find record by id, update record and delete record

--- src/routes/firRoutes.js
+++ src/routes/firRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const authMiddleware = require("../middleware/authMiddleware");
 
-const { createFIR, getAllFIRs } = require("../controllers/firController");
+const { createFIR, getAllFIRs, getFIR, updateFIR, deleteFIR } = require("../controllers/firController");
 
 router.post("/", authMiddleware, createFIR);
-
 router.get("/", authMiddleware, getAllFIRs);
+router.get("/:id", authMiddleware, getFIR);
+router.put("/:id", authMiddleware, updateFIR);
+router.delete("/:id", authMiddleware, deleteFIR);  
 
 module.exports = router;
